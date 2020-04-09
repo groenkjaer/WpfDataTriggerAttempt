ccc97d680bd8219a613622de8779361b871c9075
Fillip <fill0246@student.eadania.dk>
2020-04-09 02:54:21 +0200

    Add project files.

---
+++ WpfDataTriggerAttempt/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDataTriggerAttempt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+
+        private void btnMyButton_Click(object sender, RoutedEventArgs e)
+        {
+            SingletonBoy.Instance.BoolFlag = !SingletonBoy.Instance.BoolFlag;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ WpfDataTriggerAttempt/SingletonBoy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataTriggerAttempt
+{
+    public sealed class SingletonBoy : INotifyPropertyChanged
+    {
+        private static SingletonBoy instance = null;
+        private static readonly object padlock = new object();
+
+        private bool boolFlag = false;
+        public bool BoolFlag
+        {
+            get { return boolFlag; }
+            set
+            {
+                boolFlag = value;
+                RaisePropertyChanged("BoolFlag");
+            }
+        }
+
+        SingletonBoy() //closed
+        {
+        }
+
+        public static SingletonBoy Instance //threadsafe
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SingletonBoy();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
